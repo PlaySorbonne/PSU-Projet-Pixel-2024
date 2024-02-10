dcd68dd78a92e6ce5fadcbef08b774506999f9eb
Nassim <aa.nassim.ddr@gmail.com>
2024-02-10 23:20:28 +0100

    Input detection for keyboard and gamepad plus basic movement and jump #1

---
+++ ProjetPixel2024Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 10; //TODO : Try how it feels with force instead of velocity
+    public float jumpHeight = 10; 
+
+
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+
+    private float f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+
+    private void Update()
+    {
+        //Debug.Log(f);
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.AddForce(movementDirection * new Vector2(movementSpeed, jumpHeight));
+    }
+
+    public void OnMove(InputValue value)
+    {
+        //movementDirection.x = value.Get<float>();
+        rb.velocity= new Vector2(value.Get<float>() * movementSpeed, rb.velocity.y);
+    }
+
+    public void OnJump()
+    {
+        //movementDirection.y = 1;
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+
+
+}
