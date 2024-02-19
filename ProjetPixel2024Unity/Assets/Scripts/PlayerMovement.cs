9b0ee6c12b8ee2e894984fc426298b7a9329ee9c
Nassim <aa.nassim.ddr@gmail.com>
2024-02-19 12:28:36 +0100

    Updated movement system #1

--- ProjetPixel2024Unity/Assets/Scripts/PlayerMovement.cs
+++ ProjetPixel2024Unity/Assets/Scripts/PlayerMovement.cs
@@ -5,41 +5,89 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Vertical movement variables
+    [Header("Horizontal Movement")]
     public float movementSpeed = 10; //TODO : Try how it feels with force instead of velocity
-    public float jumpHeight = 10; 
+    public float acceleration = 7;
+    public float decceleration = 7;
+    public float velPower = 0.9f;
+    public float frictionAmount = 0.2f;
+    #endregion
 
+    #region Horizontal movement variables
+
+    [Header("Vertical Movement")]
+    public Transform groundCheck;
+    public float groundCheckSize; 
+    public float jumpForce = 15;
+    
+    #endregion
+
+    #region inputs variables
+
+    private float hInput;
+    private bool vInput;
+    private bool grounded;
+
+    #endregion
+
+    #region components variables
 
     private Rigidbody2D rb;
-    private Vector2 movementDirection;
 
-    private float f;
+    #endregion
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>(); 
     }
 
-
-    private void Update()
-    {
-        //Debug.Log(f);
-    }
-
     private void FixedUpdate()
     {
-        //rb.AddForce(movementDirection * new Vector2(movementSpeed, jumpHeight));
+
+        #region updating physics variables 
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckSize);
+
+        #endregion
+
+        #region horizontal movement
+
+        //Applying horizontal force to move the player 
+
+        float targetSpeed = hInput * movementSpeed;
+        float speedDif = targetSpeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+        rb.AddForce(movement * Vector2.right);
+        
+        //Applying force to simulate friction
+        if (grounded && Mathf.Abs(hInput) < 0.01f)
+        {
+            float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAmount));
+            amount *= Mathf.Sign(rb.velocity.x);
+            rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
+        }
+        #endregion
+
+
+
+        #region vertical movement
+
+        //TODOOOOOO UWU https://www.youtube.com/watch?v=KbtcEVCM7bw
+
+
+        #endregion
     }
 
     public void OnMove(InputValue value)
     {
-        //movementDirection.x = value.Get<float>();
-        rb.velocity= new Vector2(value.Get<float>() * movementSpeed, rb.velocity.y);
+        hInput = value.Get<float>();
     }
 
     public void OnJump()
     {
-        //movementDirection.y = 1;
-        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        vInput = true; 
     }
 
 
